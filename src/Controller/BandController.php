cfff8f0809204d99d48dde21bdaef969dce1fc3c
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-06-01 12:06:00 +0200

    form ajout groupes avec images

--- src/Controller/BandController.php
+++ src/Controller/BandController.php
@@ -7,18 +7,107 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Trait\UserInfoTrait;
 use Symfony\Bundle\SecurityBundle\Security;
+use App\Entity\Band;
+use App\Repository\BandRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\BandType;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class BandController extends AbstractController
 {
     use UserInfoTrait;
     #[Route('/admin/band', name: 'app_admin_band')]
-    public function index(Security $security): Response
+    public function index(Security $security, BandRepository $bandRepository): Response
     {
         $user = $this->getUserInfo($security);
+
+        $bands = $bandRepository->findAll();
+
+
+     
+     
+     
         return $this->render('band/index.html.twig', [
             'controller_name' => 'BandController',
             'firstName' => $user['firstName'],
-            'role' => $user['role']
+            'role' => $user['role'],
+            'bands' => $bands,
         ]);
     }
+
+#[Route('/admin/band/new', name: 'app_admin_band_new')]
+    public function add(Security $security, EntityManagerInterface $entityManagerInterface, Request $request, SluggerInterface $sluggerInterface): Response
+    {
+
+        $band = new Band();
+        $form = $this->createForm(BandType::class, $band);
+        $form->handleRequest($request);
+        $user = $this->getUserInfo($security);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+                //Gestion de l'upload de l'image
+
+                $file = $form->get('file')->getData();
+
+                   if ($file) {
+
+
+                    switch ($file->getClientMimeType()) {
+    case 'image/jpeg':
+        $extension = 'jpg'; // Utiliser l'extension .jpg pour les fichiers JPEG
+        break;
+    case 'image/png':
+        $extension = 'png'; // Utiliser l'extension .png pour les fichiers PNG
+        break;
+    case 'image/avif':
+        $extension = 'avif'; // Utiliser l'extension .avif pour les fichiers AVIF
+        break;
+    case 'image/webp':
+        $extension = 'webp'; // Utiliser l'extension .webp pour les fichiers WebP
+        break;
+    // Ajouter d'autres cas pour d'autres formats de fichiers si nécessaire
+}
+
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $sluggerInterface->slug($originalFilename);
+                $fileName = $safeFilename . '-' . uniqid() . '.' . $extension;
+
+                try {
+                    $file->move(
+                        $this->getParameter('upload_directory'),
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    // Handle exception if something happens during file upload
+                }
+
+                $fileUrl =  '/images/bands' . $fileName;
+                $band->setUrlImage($fileUrl);
+            }
+
+
+
+            $entityManagerInterface->persist($band);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('app_admin_band');
+        }
+
+        return $this->render('band/add.html.twig', [
+            'controller_name' => 'BandController',
+            'firstName' => $user['firstName'],
+            'role' => $user['role'],
+            'form' => $form->createView(),
+        ]);
+
+
+    }
+
+
+
+
 }
--- src/Form/BandType.php
+++ src/Form/BandType.php
@@ -4,10 +4,14 @@ namespace App\Form;
 
 use App\Entity\Band;
 use App\Entity\MusicStyle;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BandType extends AbstractType
 {
@@ -15,11 +19,16 @@ class BandType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('description')
-            ->add('url_image')
+            ->add('description', TextareaType::class)
+            ->add('url_image', HiddenType::class)
+            ->add('file', FileType::class, [
+                'label' => 'Image',
+                'mapped' => false,
+                'required' => false,
+            ])
             ->add('music_style', EntityType::class, [
                 'class' => MusicStyle::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
             ])
         ;
     }
